156d2aa5517cd8880caeb1a359ed730cff031de7
Lucia Espona Pernas <espona@users.noreply.github.com>
2017-04-20 15:58:02 +0200

    Update travis-build.bash

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -25,6 +25,8 @@ then
     git checkout release-v$CKANVERSION-latest
 fi
 python setup.py develop
+pip install -e "git+https://github.com/datagovuk/ckanext-hierarchy.git#egg=ckanext-hierarchy"
+pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
 cd -
 
@@ -51,7 +53,6 @@ pip install pytz==2016.4
 
 echo "Installing ckanext-envidat_theme and its requirements..."
 python setup.py develop
-pip install -e "git+https://github.com/datagovuk/ckanext-hierarchy.git#egg=ckanext-hierarchy"
 pip install -r dev-requirements.txt
 
 echo "Moving test.ini into a subdir..."
