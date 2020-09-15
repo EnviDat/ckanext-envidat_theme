678451daf1d654101a7d923cc5ec478a1c340ebe
espona <lucia.espona@gmail.com>
2020-09-15 15:54:48 +0200

    added the API key renewal

--- ckanext/envidat_theme/action.py
+++ ckanext/envidat_theme/action.py
@@ -2,6 +2,8 @@
 
 from ckan.logic import side_effect_free
 from ckan.logic.action.get import user_show
+import ckan.plugins.toolkit as toolkit
+from ckanext.passwordless import util
 
 from logging import getLogger
 
@@ -23,6 +25,11 @@ def envidat_get_user_from_context(context):
         auth_user_obj_dict = auth_user_obj.as_dict()
         user_data = user_show(context, {'id': auth_user_obj_dict['id']})
         auth_user_obj_dict["email_hash"] = user_data["email_hash"]
+
+        # renew the master key
+        apikey = util.renew_master_token(auth_user_obj_dict['name'])
+        auth_user_obj_dict["apikey"] = apikey
+
         return auth_user_obj_dict
     else:
         return {}
