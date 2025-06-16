8e843076550d23dcc4907d4bff60e3b3968a9ead
rebeccakurup <rebecca.kurup@wsl.ch>
2025-06-16 16:59:16 +0200

    refactor: remove action

--- ckanext/envidat_theme/plugin.py
+++ ckanext/envidat_theme/plugin.py
@@ -61,8 +61,10 @@ class Envidat_ThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
 
     # IActions
     def get_actions(self):
-        return {'envidat_context_user_show': action.context_user_show,
-                'envidat_get_author_data': action.envidat_get_author_data}
+        return {
+            # 'envidat_context_user_show': action.context_user_show,
+            'envidat_get_author_data': action.envidat_get_author_data
+        }
 
     # IAuthFunctions
     # The portal admin can always update
